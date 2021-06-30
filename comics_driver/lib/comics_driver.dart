abd14ab5c554be1bf5bac56c025888e8d0feb61d
Marcus Harbol <marcusharbol@new-host-2.home>
2021-06-30 14:58:39 -0700

    First commit

---
+++ comics_driver/lib/comics_driver.dart
@@ -0,0 +1,37 @@
+library comics_driver;
+
+import 'package:web_regex/web_regex.dart';
+
+// the ComicGetter class is bound to a specific website to get comics.
+// A static ComicGetter will exist for all websites and will be pointed to
+// when needed for comic retrieval
+class ComicGetter {
+
+  // then there are the regex start and end points that will go into the web_regex
+  String regexStart;
+  String regexEnd;
+  String regexBody;
+
+  // URL extension format for where to find the comic. Can use a good number of replacement tokens if needed
+  String urlFormat;
+
+  // static members for the urlFormat
+  static const String BASE_URL = '\$BASE_URL_TOKEN';
+  static const String YEAR = '\$YEAR4_TOKEN';
+  static const String MONTH_NUMBER = '\$MONTH_TOKEN';
+  static const String DATE_NUMBER = '\$DATE_TOKEN';
+
+  // this will go in the place of the comic title and will be replaced whenever we call the ComicGetter
+  // for a specific comic
+  static const String COMIC_TITLE = '\$COMIC_TITLE_IN_URL_TOKEN';
+
+  // constructor for the ComicGetter class. Uses named paramaters for readability 
+  ComicGetter({required this.urlFormat,
+               required this.regexStart, 
+               required this.regexEnd, 
+               this.regexBody = WebRegex.WILDCARD}){
+                 
+    // build the urlFormat by doing a lot of fancy replaces...
+    
+  }
+}
---
+++ web_regex/lib/src/web_regex_base.dart
@@ -0,0 +1,75 @@
+
+import 'package:http/http.dart';
+
+// class used to extract info from a website's HTML using regex in cool ways
+class WebRegex {
+
+  static const String WILDCARD = '.*';
+
+  // the url to this specific page
+  String url;
+
+  // start and end of the RegEx for whatever we want to extract
+  String regexStart;
+  String regexEnd;
+
+  // this denotes the RegEx for the body. We just use wildcard as a default
+  String bodyRegex;
+
+  // Constructor method for this WebRegex
+  WebRegex({
+    required this.url,
+    required this.regexStart,
+    required this.regexEnd, 
+    this.bodyRegex = WILDCARD,
+    });
+
+  // private method to get website HTML body
+  Future<String> _getSiteHTML() async {
+
+    // get the HTML from the input URL
+    var response = await get(Uri.parse(url));
+
+    return response.body;
+  }
+
+  // find and extract the meat from the HTML using the regex
+  Future<String> extract() async {
+
+    // first get the html
+    var html = await _getSiteHTML();
+
+    // then look for the match in the html
+    // build the regex to read from
+    var exp = RegExp(regexStart + bodyRegex + regexEnd);
+
+    // find all matches for that regex
+    var matches = exp.allMatches(html);
+
+    // use the first (hopefully only) match to make a substring and return after trimming the regexStart and regexEnd
+    // here is how you would do it in many steps:
+    /*
+
+    // find the start and end of the full regex
+    var start = matches.first.start;
+    var end   = matches.first.end;
+
+    // isolate just the base string as a substring (regex's will be literals in there)
+    var baseString = html.substring(start, end);
+
+    // use regex to eliminate the start and end regex by replacing with empty String
+    baseString = baseString.replaceAllMapped(RegExp(regexStart), (match) => '');
+
+    baseString = baseString.replaceAllMapped(RegExp(regexEnd), (match) => '');
+
+    return baseString;
+    */
+
+    // here is the same thing all in one step:
+    return html.substring(matches.first.start, matches.first.end)
+      .replaceAllMapped(RegExp(regexStart), (match) => '')
+      .replaceAllMapped(RegExp(regexEnd), (match) => '');
+ 
+  }
+
+}
